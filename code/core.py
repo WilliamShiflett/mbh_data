6a2b6435c544b6687b588e7dc83b2040ebc52441
Will Shiflett <3066319+WilliamShiflett@users.noreply.github.com>
2023-12-06 21:46:24 -0500

    Initial commit, structure, to-do list

---
+++ code/core.py
@@ -0,0 +1,7 @@
+# To do:
+
+# Capture state of https://s3.amazonaws.com/capitalbikeshare-data/index.html. Do you have the latest clean data?
+# Compare to prior state in some kind of database. Are there any new files?
+# Download new files.
+# Unzip files.
+# Put files into a database.
