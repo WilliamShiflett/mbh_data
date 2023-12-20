e15e491541423b9ef6c6d30983213eae229b2ffa
Will Shiflett <3066319+WilliamShiflett@users.noreply.github.com>
2023-12-20 00:09:07 -0500

    add selenium for rendered page

--- tests/listener_test.py
+++ tests/listener_test.py
@@ -1,17 +1,26 @@
 import pytest
-from src.listener import requester, texter
+from src.listener import requester, texter, visiter, URL
 from http import HTTPStatus
 
 #is this url still active?
 def test_url():
-    test_status = requester('https://s3.amazonaws.com/capitalbikeshare-data/index.html')
+    test_status = requester(URL)
     assert test_status.status_code == HTTPStatus.OK.value
 
 #is there anything at this url?
 def test_url_text():
-    test_url_response = requester('https://s3.amazonaws.com/capitalbikeshare-data/index.html')
+    test_url_response = requester(URL)
     test_status = texter(test_url_response)
     assert test_status is not None
 
-#can I make a hasg of what's at this url?
+#can I make a hash of what's at this url?
+def test_rendered_page():
+    test_page = visiter(URL)
+    assert visiter is not None
+
+
+
+    
+#will hash change moment by moment?
+
 
