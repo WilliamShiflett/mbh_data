1ce968ab23f56156c7c6b1fb44768a50942e67c8
Will Shiflett <3066319+WilliamShiflett@users.noreply.github.com>
2023-12-28 00:57:16 -0500

    Refactor to get all hrefs

--- src/listener.py
+++ src/listener.py
@@ -15,29 +15,32 @@ import time
 import hashlib
 import requests
 from http import HTTPStatus
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://s3.amazonaws.com/capitalbikeshare-data/index.html'
 
-def requester(url: str):
-    response = requests.get(url)
-    return response
-
-def texter(url_response):
-    url_text = url_response.text
-    return url_text
-
-def visiter(url: str):
+def page_getter(url: str):
     driver = webdriver.Chrome() 
     driver.get(url)
-    page = driver.page_source.encode('utf-8') 
-    return page
 
+    #Implment max retries
+    try:
+        # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, '.zip'))).click()
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, '.zip'))).get_attribute('href')
+        elems = driver.find_elements(By.XPATH, "//a[@href]")
+        time.sleep(5)
+    except:
+        time.sleep(5)
     
+    return elems
+
 def main():
-    text_getter_input = requester(URL)
-    text_getter_output = texter(text_getter_input)
-    visitor(URL)
+    test = page_getter(URL)
+    for e in test:
+        print(e.get_attribute('href'))
 
 if __name__=='__main__':
     main()
