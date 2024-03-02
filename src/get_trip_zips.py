a990898b94f95e44be9dd9d6034d7f47297a365a
Will Shiflett <3066319+WilliamShiflett@users.noreply.github.com>
2024-03-02 00:58:14 -0500

    refactor

--- src/get_trip_zips.py
+++ src/get_trip_zips.py
@@ -1,9 +1,9 @@
 '''
-Navigate to:
-https://s3.amazonaws.com/capitalbikeshare-data/index.html
+1. Navigate to: https://s3.amazonaws.com/capitalbikeshare-data/index.html.
+2. Capture href elements in a list after they render.
+3. Extract URLs for zip files from href elements.
 
 Get zip file URLs for each quarter's worth of trip data.
-
 '''
 import logging
 from selenium import webdriver
@@ -16,47 +16,68 @@ import time
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 MAIN_URL = 'https://s3.amazonaws.com/capitalbikeshare-data/index.html'
+ELEMENT_TYPE = "//a[@href]"
 RETRY_COUNT = 5
 ZIP_URL_LIST = []
 
-def page_crawler(url: str) -> list:
-    '''
-    1.Visits url
-    2.Waits for href elements to render
-    3.Finds all href elements
-    4.Returns URLs in href elements
-    '''
+def main():
+    
+    # 1. Navigate to: https://s3.amazonaws.com/capitalbikeshare-data/index.html.
+    driver = page_navigator(MAIN_URL)
+
+    # 2. Capture href elements in a list after they render.
+    href_elements = element_grabber(driver, ELEMENT_TYPE)
+
+    # 3. Extract URLs for zip files from href elements.
+    zip_url_list = url_extractor(href_elements)
+
+def page_navigator(url:str) -> webdriver:
 
     driver = webdriver.Chrome() 
+    driver.set_page_load_timeout(10)
     driver.get(url)
-    driver.set_page_load_timeout(20)
 
     assert driver.current_url == 'https://s3.amazonaws.com/capitalbikeshare-data/index.html'
-    logging.info(f'Successfully got {driver.current_url}')
+    
+    logging.info(f'Successfully navigated to {driver.current_url}')
 
+    return driver
+
+def element_grabber(cbs_driver:webdriver, 
+                    element_type:str) -> list:
+    
     retries = RETRY_COUNT
+
     while retries > 0:
 
         try:
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, '.zip'))).get_attribute('href')
-            elements = driver.find_elements(By.XPATH, "//a[@href]")
+            WebDriverWait(cbs_driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, '.zip'))).get_attribute('href')
+            elements = cbs_driver.find_elements(By.XPATH, element_type)
             time.sleep(2)
-            assert len(elements) > 0 
+
+            assert len(elements) > 0
+            logging.info(f'Successfully found {element_type} elements at {MAIN_URL}')
+
             break
 
         except:
             time.sleep(2)
             retries = retries - 1
-            logging.warning(f'Failed to access {url}')
+            logging.warning(f'Failed to find href elements at {MAIN_URL}')
             logging.warning(f'Retries remaining: {retries}')
-    
-    for element in elements:
+
+    return elements
+
+def url_extractor(href_list:list) -> list:
+
+    for element in href_list:
         ZIP_URL_LIST.append(element.get_attribute('href'))
 
-    return ZIP_URL_LIST
+    assert len(ZIP_URL_LIST) > 0
+    zip_url_count = len(ZIP_URL_LIST)
+    logging.info(f'Successfully found {zip_url_count} zip file URLs at {MAIN_URL}')
 
-def main():
-    page_crawler(MAIN_URL)
+    return ZIP_URL_LIST
 
 if __name__=='__main__':
     main()
